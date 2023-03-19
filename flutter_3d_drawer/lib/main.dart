591f0e8bf4f866e5ae3da58444b4db77ab1585f8
mdrahamat0505 <mdrahamat0505@gmail.com>
2023-03-19 23:02:50 +0600

    update drawer.

--- flutter_3d_drawer/lib/main.dart
+++ flutter_3d_drawer/lib/main.dart
@@ -1,115 +1,163 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show pi;
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const App(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyDrawer extends StatefulWidget {
+  final Widget child;
+  final Widget drawer;
+  const MyDrawer({
+    super.key,
+    required this.child,
+    required this.drawer,
+  });
 
-  // This widget is the root of your application.
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> with TickerProviderStateMixin {
+  late AnimationController _xControllerForChild;
+  late Animation<double> _yRotationAnimationForChild;
+
+  late AnimationController _xControllerForDrawer;
+  late Animation<double> _yRotationAnimationForDrawer;
+
+  @override
+  void initState() {
+    super.initState();
+    _xControllerForChild = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _yRotationAnimationForChild = Tween<double>(
+      begin: 0,
+      end: -pi / 2,
+    ).animate(_xControllerForChild);
+
+    _xControllerForDrawer = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _yRotationAnimationForDrawer = Tween<double>(
+      begin: pi / 2.7,
+      end: 0,
+    ).animate(_xControllerForDrawer);
+  }
+
+  @override
+  void dispose() {
+    _xControllerForChild.dispose();
+    _xControllerForDrawer.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    final screenWidth = MediaQuery.of(context).size.width;
+    final maxDrag = screenWidth * 0.8;
+
+    return GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        final delta = details.delta.dx / maxDrag;
+        _xControllerForChild.value += delta;
+        _xControllerForDrawer.value += delta;
+      },
+      onHorizontalDragEnd: (details) {
+        if (_xControllerForChild.value < 0.5) {
+          _xControllerForChild.reverse();
+          _xControllerForDrawer.reverse();
+        } else {
+          _xControllerForChild.forward();
+          _xControllerForDrawer.forward();
+        }
+      },
+      child: AnimatedBuilder(
+        animation: Listenable.merge([
+          _xControllerForChild,
+          _xControllerForDrawer,
+        ]),
+        builder: (context, child) {
+          return Stack(
+            children: [
+              Container(
+                color: const Color(0xFF1a1b26),
+              ),
+              Transform(
+                alignment: Alignment.centerLeft,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..translate(_xControllerForChild.value * maxDrag)
+                  ..rotateY(_yRotationAnimationForChild.value),
+                child: widget.child,
+              ),
+              Transform(
+                alignment: Alignment.centerRight,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..translate(
+                      -screenWidth + _xControllerForDrawer.value * maxDrag)
+                  ..rotateY(_yRotationAnimationForDrawer.value),
+                child: widget.drawer,
+              ),
+            ],
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyDrawer(
+      drawer: Material(
+        child: Container(
+          color: const Color(0xff24283b),
+          child: ListView.builder(
+            padding: const EdgeInsets.only(left: 100, top: 100),
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('Item $index'),
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Drawer'),
+        ),
+        body: Container(
+          color: const Color(0xff414868),
+        ),
+      ),
     );
   }
 }
