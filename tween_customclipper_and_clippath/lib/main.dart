b1c93678f300b90e4631d2192dfcc29b44794a6d
mdrahamat0505 <mdrahamat0505@gmail.com>
2023-03-24 00:25:59 +0600

    update tween clippath.

--- tween_customclipper_and_clippath/lib/main.dart
+++ tween_customclipper_and_clippath/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'dart:math' show pi, cos, sin;
+import 'dart:math' as math;
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() {
   runApp(
@@ -25,56 +31,42 @@ class App extends StatelessWidget {
   }
 }
 
-class Polygon extends CustomPainter {
-  final int sides;
-
-  Polygon({
-    required this.sides,
-  });
+class CircleClipper extends CustomClipper<Path> {
+  const CircleClipper();
 
   @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.blue
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 3;
+  Path getClip(Size size) {
+    var path = Path();
 
-    final path = Path();
-
-    final center = Offset(size.width / 2, size.height / 2);
-    final angle = (2 * pi) / sides;
-
-    final angles = List.generate(sides, (index) => index * angle);
-
-    final radius = size.width / 2;
-
-    /*
-    x = center.x + radius * cos(angle)
-    y = center.y + radius * sin(angle)
-    */
-
-    path.moveTo(
-      center.dx + radius * cos(0),
-      center.dy + radius * sin(0),
+    final rect = Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: size.width / 2,
     );
 
-    for (final angle in angles) {
-      path.lineTo(
-        center.dx + radius * cos(angle),
-        center.dy + radius * sin(angle),
-      );
-    }
+    path.addOval(rect);
 
-    path.close();
-
-    canvas.drawPath(path, paint);
+    return path;
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
-      oldDelegate is Polygon && oldDelegate.sides != sides;
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
+
+/*
+0xFFFFFFFF
+A R G B = 32 bits
+  A = Alpha (0 - 255) - 8 bits
+  R = Red (0 - 255) - 8 bits
+  G = Green (0 - 255) - 8 bits
+  B = Blue (0 - 255) - 8 bits
+*/
+
+Color getRandomColor() => Color(
+      0xFF000000 +
+          math.Random().nextInt(
+            0x00FFFFFF,
+          ),
+    );
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -83,104 +75,41 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  late AnimationController _sidesController;
-  late Animation<int> _sidesAnimation;
-
-  late AnimationController _radiusController;
-  late Animation<double> _radiusAnimation;
-
-  late AnimationController _rotationController;
-  late Animation<double> _rotationAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _sidesController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
-    _sidesAnimation = IntTween(
-      begin: 3,
-      end: 10,
-    ).animate(_sidesController);
-
-    _radiusController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
-    _radiusAnimation = Tween(
-      begin: 20.0,
-      end: 400.0,
-    )
-        .chain(
-          CurveTween(
-            curve: Curves.bounceInOut,
-          ),
-        )
-        .animate(_radiusController);
-
-    _rotationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
-
-    _rotationAnimation = Tween(
-      begin: 0.0,
-      end: 2 * pi,
-    )
-        .chain(
-          CurveTween(
-            curve: Curves.easeInOut,
-          ),
-        )
-        .animate(_rotationController);
-  }
-
-  @override
-  void dispose() {
-    _sidesController.dispose();
-    _radiusController.dispose();
-    _rotationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _sidesController.repeat(reverse: true);
-    _radiusController.repeat(reverse: true);
-    _rotationController.repeat(reverse: true);
-  }
+class _HomePageState extends State<HomePage> {
+  var _color = getRandomColor();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AnimatedBuilder(
-          animation: Listenable.merge(
-            [
-              _sidesController,
-              _radiusController,
-              _rotationController,
-            ],
-          ),
-          builder: (context, child) {
-            return Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.identity()
-                ..rotateX(_rotationAnimation.value)
-                ..rotateY(_rotationAnimation.value)
-                ..rotateZ(_rotationAnimation.value),
-              child: CustomPaint(
-                painter: Polygon(sides: _sidesAnimation.value),
-                child: SizedBox(
-                  width: _radiusAnimation.value,
-                  height: _radiusAnimation.value,
+        child: ClipPath(
+          clipper: const CircleClipper(),
+          child: TweenAnimationBuilder(
+            tween: ColorTween(
+              begin: getRandomColor(),
+              end: _color,
+            ),
+            onEnd: () {
+              setState(() {
+                _color = getRandomColor();
+              });
+            },
+            duration: const Duration(seconds: 1),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              color: Colors.red,
+            ),
+            builder: (context, Color? color, child) {
+              return ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  color!,
+                  BlendMode.srcATop,
                 ),
-              ),
-            );
-          },
+                child: child,
+              );
+            },
+          ),
         ),
       ),
     );
